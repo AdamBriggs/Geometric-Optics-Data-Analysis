a84761cad6bdc7cf14c059f68e0ffb320183ba6f
Adam Briggs <adambriggs@dhcp-10-5-25-49.wireless.rochester.edu>
2019-11-09 14:02:02 -0500

    Changes

---
+++ R/CAS352-ResearchProject.R
@@ -0,0 +1,97 @@
+# Adam Briggs
+# CAS 352 Reserach Project Data Analysis
+
+# Import Libraries
+library(googlesheets4)
+library(googledrive)
+library(corrgram)
+
+data <- read_sheet("https://docs.google.com/spreadsheets/d/1buK0ZxyTiJ1rPrUbCSbql0RezCqn8-XJJ-b40xtPVSw/edit#gid=827708374")
+names(data) <- c("Timestamp","Email","Understand Before","Understand After","Varied","Confidence","Conclusion","Teamwork",
+                   "Group Work","Satisfaction","Benifit","Additional Thoughts","Item","Leader")
+# Removes unused rows from
+data <- data[-c(1:6),]
+
+# Plotting Number vs String
+plot(factor(data$Item),data$Confidence)
+
+# Perform Principal Component Analysis on Data
+pca <- prcomp(data[3:11], scale = TRUE)
+
+# Display the results of PCA
+pca
+summary(pca)
+
+# BiPlot of PCA Data
+biplot(pca)
+
+# Append the results of PC1 and PC2 to each variable in main data set
+data2 <- cbind(data, pca$x[,1:2])
+
+# Plotting with GGPLOT ---
+library(ggplot2)
+# ------------------------- Plotting PCA2 v PCA1
+# Plots calculated PCA values against each other. The ellipses  are calculated with a multivariate
+# t-distribution and the confidance interval represents 1 standard deviation (68%) of the data.
+ggplot(data2, aes(PC1,PC2, col = Item, fill = Item)) + 
+  stat_ellipse(geom = "polygon", col = "black", alpha = 0.5, type = "t", level=.68) + 
+  geom_point(shape = 21, col = "black") +
+  scale_fill_discrete(name = "Brainstorming Type", labels = c("Group", "Individual")) +
+  ggtitle("Principal Component Confidence Interval of Brainstorming", subtitle = "Confidence Interval of One Standard Deviation") +
+  theme(plot.title = element_text(lineheight=.8, face="bold")) +
+  theme(plot.title = element_text(hjust = 0.5),plot.subtitle = element_text(hjust = 0.5)) +
+  xlab("PC1 (Arb. Unit)") + ylab("PC2 (Arb. Unit)") +
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank())
+# -------------------------
+
+# Correlations Between Variables and PCs
+# 
+corr <- cor(data[3:11],data2[15:16])
+corr
+
+# Plotting Histogram For Understanding Before
+BS <- as.data.frame(data$`Understand Before`[1:21])
+colnames(BS) <- c("Before")
+BS$type <- 'Solo'
+
+BG <-as.data.frame(data$`Understand Before`[22:37])
+colnames(BG) <- c("Before")
+BG$type <- 'Group'
+
+groupval <- rbind(BS, BG)
+ggplot(groupval, aes(Before, fill = type)) +
+  geom_density(alpha = 0.7)
+
+# Plotting Histogram For Understanding After
+BS <- as.data.frame(data$`Understand After`[1:10])
+colnames(BS) <- c("After")
+BS$type <- 'Solo'
+
+BG <-as.data.frame(data$`Understand After`[11:20])
+colnames(BG) <- c("After")
+BG$type <- 'Group'
+
+groupval <- rbind(BS, BG)
+ggplot(groupval, aes(After, fill = type)) +
+  geom_density(alpha = 0.5)
+
+
+
+# Histogram Example
+# Interleaved histograms
+ggplot(data2, aes(x=Varied, fill=Item)) +
+  geom_histogram(position='dodge',binwidth = 1) +
+  scale_fill_discrete(name = "Brainstorming Type", labels = c("Group", "Individual")) +
+  xlim(0,10) + ylim(0,10) +
+  ggtitle("Histogram of Brainstorming Results", subtitle = "Variation of Ideas") +
+  theme(plot.title = element_text(lineheight=.8, face="bold")) +
+  theme(plot.title = element_text(hjust = 0.5),plot.subtitle = element_text(hjust = 0.5)) +
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank()) 
