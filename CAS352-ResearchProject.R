55f16a3e20c741dcc83430ab053d750467c62852
Adam Briggs <adambriggs@dhcp-10-5-25-49.wireless.rochester.edu>
2019-11-09 14:03:20 -0500

    New Updates

--- CAS352-ResearchProject.R
+++ CAS352-ResearchProject.R
@@ -4,17 +4,16 @@
 # Import Libraries
 library(googlesheets4)
 library(googledrive)
+library(corrgram)
 
 data <- read_sheet("https://docs.google.com/spreadsheets/d/1buK0ZxyTiJ1rPrUbCSbql0RezCqn8-XJJ-b40xtPVSw/edit#gid=827708374")
 names(data) <- c("Timestamp","Email","Understand Before","Understand After","Varied","Confidence","Conclusion","Teamwork",
                    "Group Work","Satisfaction","Benifit","Additional Thoughts","Item","Leader")
-
+# Removes unused rows from
 data <- data[-c(1:6),]
-
 
 # Plotting Number vs String
 plot(factor(data$Item),data$Confidence)
-
 
 # Perform Principal Component Analysis on Data
 pca <- prcomp(data[3:11], scale = TRUE)
@@ -41,7 +40,7 @@ ggplot(data2, aes(PC1,PC2, col = Item, fill = Item)) +
   ggtitle("Principal Component Confidence Interval of Brainstorming", subtitle = "Confidence Interval of One Standard Deviation") +
   theme(plot.title = element_text(lineheight=.8, face="bold")) +
   theme(plot.title = element_text(hjust = 0.5),plot.subtitle = element_text(hjust = 0.5)) +
-  xlab("PC1 (Arb. Unit)") + ylab("PC1 (Arb. Unit)") +
+  xlab("PC1 (Arb. Unit)") + ylab("PC2 (Arb. Unit)") +
   theme(axis.line = element_line(colour = "black"),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
@@ -50,28 +49,49 @@ ggplot(data2, aes(PC1,PC2, col = Item, fill = Item)) +
 # -------------------------
 
 # Correlations Between Variables and PCs
-# Note that the correlation needs to be defined after both sets of data are implimented. 
-# This just shows which quetsions have the highest correlation with the PCA analysis of the seperability.
-corr <- cor(data2[,3:11],data2[,15:16])
+# 
+corr <- cor(data[3:11],data2[15:16])
 corr
 
 # Plotting Histogram For Understanding Before
-BS <- as.data.frame(data$`Understand Before`[1:20])
+BS <- as.data.frame(data$`Understand Before`[1:21])
 colnames(BS) <- c("Before")
 BS$type <- 'Solo'
 
-BG <-as.data.frame(data$`Understand Before`[21:40])
+BG <-as.data.frame(data$`Understand Before`[22:37])
 colnames(BG) <- c("Before")
 BG$type <- 'Group'
 
 groupval <- rbind(BS, BG)
 ggplot(groupval, aes(Before, fill = type)) +
-  geom_density(alpha = 0.7, fill = 'steelblue')
+  geom_density(alpha = 0.7)
+
+# Plotting Histogram For Understanding After
+BS <- as.data.frame(data$`Understand After`[1:10])
+colnames(BS) <- c("After")
+BS$type <- 'Solo'
+
+BG <-as.data.frame(data$`Understand After`[11:20])
+colnames(BG) <- c("After")
+BG$type <- 'Group'
+
+groupval <- rbind(BS, BG)
+ggplot(groupval, aes(After, fill = type)) +
+  geom_density(alpha = 0.5)
 
 
 
-
-# ------------ Saving Images
-# plots.dir.path <- list.files(tempdir(), pattern="rs-graphics", full.names = TRUE); 
-# plots.png.paths <- list.files(plots.dir.path, pattern=".png", full.names = TRUE)
-# file.copy(from=plots.png.paths, to="/Users/adambriggs/Dropbox/College/Fall 2019/CAS 352/Research Project/Figures")
+# Histogram Example
+# Interleaved histograms
+ggplot(data2, aes(x=Varied, fill=Item)) +
+  geom_histogram(position='dodge',binwidth = 1) +
+  scale_fill_discrete(name = "Brainstorming Type", labels = c("Group", "Individual")) +
+  xlim(0,10) + ylim(0,10) +
+  ggtitle("Histogram of Brainstorming Results", subtitle = "Variation of Ideas") +
+  theme(plot.title = element_text(lineheight=.8, face="bold")) +
+  theme(plot.title = element_text(hjust = 0.5),plot.subtitle = element_text(hjust = 0.5)) +
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank()) 
